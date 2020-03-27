6cb264ab94459a00e79e2bbcfeb031f0f9d65048
Michael O'Keefe <mokeefe@mitre.org>
2020-03-27 12:13:14 -0400

    Remove unused import

--- src/main/java/org/mitre/synthea/engine/Generator.java
+++ src/main/java/org/mitre/synthea/engine/Generator.java
@@ -15,7 +15,6 @@ import java.lang.reflect.Type;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;

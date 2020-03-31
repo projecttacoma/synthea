fd6c6ed7ec2f4750e1b1da5b254531b03b48f57c
Matthew Gramigna <mgramigna@mitre.org>
2020-03-31 09:52:08 -0400

    Address java checkstyle warnings

--- src/main/java/org/mitre/synthea/helpers/ValueSetResolver.java
+++ src/main/java/org/mitre/synthea/helpers/ValueSetResolver.java
@@ -1,20 +1,20 @@
 package org.mitre.synthea.helpers;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.ValueSet;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.json.simple.JSONObject;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.ValueSet;
-import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.parser.IParser;
 
 /*
  * Helper class for resolving FHIR ValueSet resources from a JSON file
@@ -24,8 +24,13 @@ public class ValueSetResolver {
   private IParser fhirParser;
   private String filePath;
 
-  final Pattern OID_REGEX = Pattern.compile("([0-9]+\\.)+[0-9]+");
+  static final Pattern OID_REGEX = Pattern.compile("([0-9]+\\.)+[0-9]+");
 
+  /**
+   * Construct a new ValueSetResolver object.
+   *
+   * @param filePath absolute path to a Bundle of ValueSets as a JSON file
+   */
   public ValueSetResolver(String filePath) {
     FhirContext ctx = FhirContext.forR4();
     this.jsonParser = new JSONParser();
@@ -40,8 +45,10 @@ public class ValueSetResolver {
     return bundle;
   }
 
-  private String matchOID(String input) {
-    if (input == null) return null;
+  private String matchOid(String input) {
+    if (input == null) {
+      return null;
+    }
 
     Matcher m = OID_REGEX.matcher(input);
 
@@ -53,36 +60,37 @@ public class ValueSetResolver {
   }
 
   /**
-   * Get ValueSet resource that match an OID in the list of desired ones
+   * Get ValueSet resource that match an OID in the list of desired ones.
    * 
-   * @param desiredOIDs List of OIDs to match against
+   * @param desiredOids List of OIDs to match against
    * @return List of ValueSet resources that match the desired OIDs
    * @throws ParseException when unable to parse ValueSet bundle into JSON
    * @throws IOException when unable to read in file
    * @throws FileNotFoundException when provided an invalid path to ValueSet Bundle
    * @throws RuntimeException when ValueSet is not found
    */
-  public List<ValueSet> getValueSets(List<String> desiredOIDs) throws FileNotFoundException, IOException, ParseException {
+  public List<ValueSet> getValueSets(List<String> desiredOids)
+      throws FileNotFoundException, IOException, ParseException {
     Bundle vsetBundle = this.parseValueSetBundle();
     List<ValueSet> matchingValueSets = new ArrayList<ValueSet>();
 
     // Check each identifier and add to list if matches
-    for (String oid : desiredOIDs) {
-      String desiredOID = this.matchOID(oid);
+    for (String oid : desiredOids) {
+      String desiredOid = this.matchOid(oid);
 
-      boolean foundMatchingOID = false;
+      boolean foundMatchingOid = false;
       for (BundleEntryComponent e : vsetBundle.getEntry()) {
         ValueSet vset = (ValueSet) e.getResource();
-        String knownOID = this.matchOID(vset.getIdentifierFirstRep().getValue());
+        String knownOid = this.matchOid(vset.getIdentifierFirstRep().getValue());
 
-        if (knownOID != null && knownOID.equals(desiredOID)) {
+        if (knownOid != null && knownOid.equals(desiredOid)) {
           matchingValueSets.add(vset);
-          foundMatchingOID = true;
+          foundMatchingOid = true;
           break;
         }
       }
 
-      if (!foundMatchingOID) {
+      if (!foundMatchingOid) {
         throw new RuntimeException(String.format("Could not resolve ValueSet %s", oid));
       }
     }

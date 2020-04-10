1586084ee93bc8a775e62a5090239830332d1dcc
Matthew Gramigna <mgramigna@mitre.org>
2020-04-10 14:01:04 -0400

    Add parsing of all bundles in terminology directory; update lint

--- src/main/java/org/mitre/synthea/helpers/ValueSetResolver.java
+++ src/main/java/org/mitre/synthea/helpers/ValueSetResolver.java
@@ -6,10 +6,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.Bundle.BundleType;
 import org.hl7.fhir.r4.model.ValueSet;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -21,32 +28,59 @@ import org.json.simple.parser.ParseException;
 public class ValueSetResolver {
   private JSONParser jsonParser;
   private IParser fhirParser;
-  private String filePath;
+
+  // Bundle containing valuesets for all measures
   private Bundle vsetBundle;
 
   static final Pattern OID_REGEX = Pattern.compile("([0-9]+\\.)+[0-9]+");
 
   /**
    * Construct a new ValueSetResolver object.
-   *
-   * @param filePath absolute path to a Bundle of ValueSets as a JSON file
-   * @throws ParseException
-   * @throws IOException
-   * @throws FileNotFoundException
    */
-  public ValueSetResolver() throws FileNotFoundException, IOException, ParseException {
+  public ValueSetResolver() {
     FhirContext ctx = FhirContext.forR4();
     this.jsonParser = new JSONParser();
     this.fhirParser = ctx.newJsonParser();
-    this.filePath = new File("").getAbsolutePath().concat("/src/main/resources/terminology/exm130/valuesets.json");
-    this.vsetBundle = parseValueSetBundle();
+    this.vsetBundle = new Bundle();
+    this.vsetBundle.setType(BundleType.TRANSACTION);
+
+    // If IO is thrown, then the walk on the terminology directory could not be performed
+    try {
+      initBundle();
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException("Could not initialize ValueSet bundles."
+        + "Ensure src/main/resources/terminology has proper contents");
+    }
   }
 
-  private Bundle parseValueSetBundle() throws FileNotFoundException, IOException, ParseException {
-    JSONObject jsonBundle = (JSONObject) this.jsonParser.parse(new FileReader(this.filePath));
-    String bundleString = jsonBundle.toString();
-    Bundle bundle = fhirParser.parseResource(Bundle.class, bundleString);
-    return bundle;
+  private void initBundle() throws IOException {
+    String filePath = new File("").getAbsolutePath().concat("/src/main/resources/terminology");
+    List<BundleEntryComponent> entries = new ArrayList<BundleEntryComponent>();
+    Stream<Path> paths = Files.walk(Paths.get(filePath));
+    paths.filter(p -> {
+      return Files.isRegularFile(p) && p.getFileName().toString().endsWith(".json");
+    }).forEach(f -> {
+      JSONObject jsonBundle;
+      try {
+        // Add this Bundle's entry to the master Bundle
+        jsonBundle = (JSONObject) this.jsonParser.parse(new FileReader(f.toFile()));
+        String bundleString = jsonBundle.toString();
+        Bundle currentBundle = fhirParser.parseResource(Bundle.class, bundleString);
+        entries.addAll(currentBundle.getEntry());
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        throw new RuntimeException("File " + f.toString() + " could not be found");
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException("Could not read file " + f.toString());
+      } catch (ParseException e) {
+        e.printStackTrace();
+        throw new RuntimeException("Could not parse " + f.toString() + " as JSON");
+      }
+    });
+    paths.close();
+    this.vsetBundle.setEntry(entries);
   }
 
   private String matchOid(String input) {
@@ -63,6 +97,11 @@ public class ValueSetResolver {
     return null;
   }
 
+  /**
+   * Retrieve a FHIR ValueSet from the master Bundle.
+   * @param oid the desired OID to retreive
+   * @return the corresponding ValueSet, null if it does not exist
+   */
   public ValueSet getValueSet(String oid) {
     String desiredOid = this.matchOid(oid);
     for (BundleEntryComponent e : vsetBundle.getEntry()) {
